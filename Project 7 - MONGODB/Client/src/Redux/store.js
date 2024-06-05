5e49c00755513c54979cf9b3230b43babb794878
Chirag-Devda <devdachirag09@gmail.com>
2024-06-05 20:49:21 +0530

    Removed Redux

--- Project 7 - MONGODB/Client/src/Redux/store.js
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import EmailReducer from "../features/UserrEmail/UserEmailSlice";
-export const store = configureStore({
-  reducer: EmailReducer,
-});
--- Project 7 - MONGODB/Client/src/main.jsx
+++ Project 7 - MONGODB/Client/src/main.jsx
@@ -8,16 +8,12 @@ import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
 import { theme } from "./theme/index.js";
 import AuthProivder from "./provider/AuthProvider.jsx";
-import { Provider } from "react-redux";
-import { store } from "./Redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AuthProivder>
-        <ChakraProvider theme={theme}>
-          <App />
-        </ChakraProvider>
-      </AuthProivder>
-    </Provider>
+    <AuthProivder>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </AuthProivder>
   </React.StrictMode>
 );
