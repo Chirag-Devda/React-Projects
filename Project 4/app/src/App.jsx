32de6d9de16bc5945a347d040a277632bafc8508
Chirag-Devda <devdachirag09@gmail.com>
2024-06-24 15:49:32 +0530

    small changes

--- Project 4/app/src/App.jsx
+++ Project 4/app/src/App.jsx
@@ -91,6 +91,7 @@ const App = () => {
             </div>
             <div className="search">
               <input
+                name="search"
                 onChange={handlesearch}
                 type="search"
                 placeholder="Search foods"
